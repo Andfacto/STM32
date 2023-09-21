44c4f5056607f575306a1dbe2bbc047dfa811cd7
Andfacto <145367366+Andfacto@users.noreply.github.com>
2023-09-21 13:44:19 +0300

    Update LM75A.c

--- LM75A/LM75A.c
+++ LM75A/LM75A.c
@@ -37,21 +37,19 @@ float LM75A_GetTemperature(LM75A* lm75a)
 		value &= ~((1 << 5) | (1 << 6));
 	else if(lm75a->mode == LM75A_DEC_25) 
 		value &= ~(1 << 5);
-		
+	
 	return value / 256.0;
 }
 
 float LM75A_GetHystTemperature(LM75A* lm75a)
 {
 	uint16_t value = LM75A_Read16bitReg(lm75a, LM75A_THYST_REG);
-	
 	return value / 256.0;
 }
 
 float LM75A_GetOSTemperature(LM75A* lm75a)
 {
 	uint16_t value = LM75A_Read16bitReg(lm75a, LM75A_TOS_REG);
-	
 	return value / 256.0;
 }
 
@@ -78,14 +76,12 @@ void LM75A_SetConfig(LM75A* lm75a, uint8_t value)
 void LM75A_Write8bitReg(LM75A* lm75a, uint8_t reg, uint8_t value) 
 {
 	uint8_t buffer[2] = {reg, value};	
-	
 	HAL_I2C_Master_Transmit(lm75a->hi2c, lm75a->adress << 1, buffer, 2, LM75A_I2C_TIMEOUT);
 }
 
 void LM75A_Write16bitReg(LM75A* lm75a, uint8_t reg, uint16_t value) 
 {
 	uint8_t buffer[3] = {reg, value >> 8, value};	
-	
 	HAL_I2C_Master_Transmit(lm75a->hi2c, lm75a->adress << 1, buffer, 3, LM75A_I2C_TIMEOUT);
 }
 
