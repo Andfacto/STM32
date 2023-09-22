e384bc03da7c160325b663608d5ac39269fdb83b
Andfacto <145367366+Andfacto@users.noreply.github.com>
2023-09-23 01:31:25 +0300

    Update LM75A.h

--- LM75A/LM75A.h
+++ LM75A/LM75A.h
@@ -20,9 +20,9 @@ typedef enum {
 } LM75A_DecimalMode;
 
 typedef struct {
-	I2C_HandleTypeDef* hi2c;
-	uint8_t adress;
-	LM75A_DecimalMode mode;
+	I2C_HandleTypeDef* _hi2c;
+	uint8_t _adress;
+	LM75A_DecimalMode _mode;
 } LM75A;
 
 void LM75A_Init(LM75A* lm75a, I2C_HandleTypeDef* hi2c, uint8_t adress, LM75A_DecimalMode mode);
@@ -41,10 +41,10 @@ float LM75A_GetOSTemperature(LM75A* lm75a);
 void LM75A_SetOSTemperature(LM75A* lm75a, float temperature);
 
 uint8_t LM75A_GetConfig(LM75A* lm75a);
-void LM75A_SetConfig(LM75A* lm75a, uint8_t value);
+void LM75A_SetConfig(LM75A* lm75a, uint8_t data);
 
-void LM75A_Write8bitReg(LM75A* lm75a, uint8_t reg, uint8_t value);
-void LM75A_Write16bitReg(LM75A* lm75a, uint8_t reg, uint16_t value);
+void LM75A_Write8bitReg(LM75A* lm75a, uint8_t reg, uint8_t data);
+void LM75A_Write16bitReg(LM75A* lm75a, uint8_t reg, uint16_t data);
 
 uint8_t LM75A_Read8bitReg(LM75A* lm75a, uint8_t reg);
 uint16_t LM75A_Read16bitReg(LM75A* lm75a, uint8_t reg);
